825a9904db977647681118f2cf31761d4d246c17
Tippedbike <113185655+Tippedbike@users.noreply.github.com>
2023-02-13 09:07:45 -0500

    13 done

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -6,9 +6,11 @@ using UnityEngine;
 public class RubyController : MonoBehaviour
 {
     public float speed = 3.0f;
-    public GameObject projectilePrefab;
+    
     public int maxHealth = 5;
-
+    
+    public GameObject projectilePrefab;
+    
     public int health { get { return currentHealth; }}
     int currentHealth;
     
@@ -56,10 +58,11 @@ public class RubyController : MonoBehaviour
             if (invincibleTimer < 0)
                 isInvincible = false;
         }
+        
         if(Input.GetKeyDown(KeyCode.C))
-{
-   Launch();
-}
+        {
+            Launch();
+        }
     }
     
     void FixedUpdate()
@@ -75,7 +78,6 @@ public class RubyController : MonoBehaviour
     {
         if (amount < 0)
         {
-            animator.SetTrigger("Hit");
             if (isInvincible)
                 return;
             
@@ -84,17 +86,17 @@ public class RubyController : MonoBehaviour
         }
         
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
-        Debug.Log(currentHealth + "/" + maxHealth);
+        
+        UIHealthBar.instance.SetValue(currentHealth / (float)maxHealth);
     }
+    
     void Launch()
     {
-    GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
+        GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
 
-    Projectile projectile = projectileObject.GetComponent<Projectile>();
-    projectile.Launch(lookDirection, 300);
+        Projectile projectile = projectileObject.GetComponent<Projectile>();
+        projectile.Launch(lookDirection, 300);
 
-    animator.SetTrigger("Launch");
+        animator.SetTrigger("Launch");
     }
-} 
-
-
+}
